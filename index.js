865111d61cfb1ab2a301af9e0baa2de2d3165cb0
diogoh04 <diogoh.o.92@gmail.com>
2024-10-10 19:49:56 -0300

    quarto commit

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-let = "Hulk";
+let nomeHeroi = "Hulk";
+let xpHulk = 5800;
+let nivel = "";
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {                                           // xpHeroi > 10000
+    nivel = "Radiante";
+}
+console.log("O Herói de nome" + nomeHeroi + "está no nível " + nivel);
 
 
-
-console.log("O Herói de nome {} está no nível de {}");
